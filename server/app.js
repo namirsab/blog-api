46a32ba50ecc55704f326af2d81b612d54460468
Namir Sayed-Ahmad <namirsab@gmail.com>
2021-04-13 11:14:47 +0200

    Add endpoints to handle reading, creating and deleting

--- server/app.js
+++ server/app.js
@@ -13,6 +13,57 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/posts", (req, res) => {
+  db.findAll()
+    .then((data) => {
+      res.status(200);
+      res.json(data);
+    })
+    .catch((error) => {
+      res.status(500);
+      res.json(error);
+    });
+});
+
+app.get("/posts/:id", (req, res) => {
+  const { id } = req.params;
+
+  db.findById(id).then((post) => {
+    if (post) {
+      res.status(200);
+      res.json(post);
+    } else {
+      res.status(404);
+      res.send({
+        error: `Post with id: ${id} not found`,
+      });
+    }
+  });
+});
+
+app.delete("/posts/:id", (req, res) => {
+  const { id } = req.params;
+
+  db.deleteById(id)
+    .then(() => {
+      res.status(200);
+      res.send();
+    })
+    .catch((error) => {
+      res.status(500);
+      res.json({
+        error: "Internal server error",
+      });
+    });
+});
+
+app.post("/posts", (req, res) => {
+  db.insert(req.body).then((newPost) => {
+    res.status(201);
+    res.json(newPost);
+  });
+});
+
 app.listen(4000, () => {
   console.log("Listening on port 4000");
 });
