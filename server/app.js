1aee83d5a1ebf93b8a5d75967e08ecd723da5cb5
Namir Sayed-Ahmad <namirsab@gmail.com>
2021-04-13 11:46:36 +0200

    Add comments and remove unused middleware

--- server/app.js
+++ server/app.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const db = require("./lib/db");
 
+/*
+  We create an express app calling
+  the express function.
+*/
 const app = express();
 
+/*
+  We setup middleware to:
+  - parse the body of the request to json for us
+  https://expressjs.com/en/guide/using-middleware.html
+*/
 app.use(express.json());
-app.use(express.query());
 
+/*
+  Endpoint to handle GET requests to the root URI "/"
+*/
 app.get("/", (req, res) => {
   res.json({
     "/posts": "read and create new posts",
@@ -13,6 +24,10 @@ app.get("/", (req, res) => {
   });
 });
 
+/*
+  We have to start the server. We make it listen on the port 4000
+
+*/
 app.listen(4000, () => {
-  console.log("Listening on port 4000");
+  console.log("Listening on http://localhost:4000");
 });
