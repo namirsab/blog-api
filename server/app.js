3dde4194ff81a7d5dfc91ed42540aae5e9a9d60f
Namir Sayed-Ahmad <namirsab@gmail.com>
2021-04-15 15:00:48 +0200

    Add cors and custom logger middleware

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const db = require("./lib/db");
 
 /*
@@ -13,6 +14,12 @@ const app = express();
   https://expressjs.com/en/guide/using-middleware.html
 */
 app.use(express.json());
+app.use(cors());
+app.use((req, res, next) => {
+  const { method, url } = req;
+  console.log(`${method} ${url}`);
+  next();
+});
 
 /*
   Endpoint to handle GET requests to the root URI "/"
